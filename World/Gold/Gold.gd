08fb7f37fde3917cc1fef7483e89d5e645573c55
AndreasHolt <aholt21@student.aau.dk>
2023-11-25 21:49:31 +0100

    ui etc

--- World/Gold/Gold.gd
+++ World/Gold/Gold.gd
@@ -1,8 +1,8 @@
 extends StaticBody2D
 @onready var animation_player = $AnimationPlayer
 @onready var area_detection = %AreaDetection
-@onready var mining_timer = %MiningTimer
-var count = 3
+
+var count = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,19 +18,20 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body: Player):
-	if body.mining:
+
 	
-		while count > 0:
-			await get_tree().create_timer(1).timeout
+	while count > 0:
+		if body.mining:
+			await get_tree().create_timer(1.25).timeout
 			animation_player.play("Destroyed")
 			count -= 1
 			decrease_gold_size()
 
-		queue_free()
-		
-		
-		
-		print(body)
+	queue_free()
+	
+	
+	
+	print(body)
 		
 		
 	pass # Replace with function body.
@@ -40,6 +41,6 @@ func _on_area_2d_body_entered(body: Player):
 
 func decrease_gold_size():
 	count -= 1
-	Global.total_player_gold += 25
+	Global.total_player_gold += 10
 	self.scale = Vector2(scale.x - 0.1, scale.y - 0.1)
 	pass # Replace with function body.
