4b6be1efbd028b038bae7bae5c897463218b1a87
AndreasHolt <aholt21@student.aau.dk>
2023-11-25 23:19:31 +0100

    Whiskey and more

--- World/CharacterBody2D.gd
+++ World/CharacterBody2D.gd
@@ -30,10 +30,11 @@ var dynamite = preload("res://Throwables/dynamite.tscn")
 func _physics_process(delta):
 	player_movement(delta)
 	enemy_attack()
+	update_health()
 	
-	if health <= 0 :
+	if Global.player_health <= 0 :
 		player_alive = false 
-		health = 0
+		Global.player_health = 0
 		print("deadge")
 		# end screen
 
@@ -135,3 +136,18 @@ func _on_attack_cooldown_timeout():
 
 func player():
 	pass
+
+func update_health():
+	var healthbar = $healthbar
+	healthbar.value = Global.player_health
+	
+	if Global.player_health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+		
+	if Global.player_health < 0:
+		Global.player_health = 0
+		
+		
+
