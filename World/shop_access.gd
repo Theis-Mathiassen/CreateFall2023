d059d5236bfd020ca3340d955cd2ce5ac27fbad2
Theis Mathiassen <theis.mathiassen@gmail.com>
2023-11-26 03:35:42 +0100

    Fixed door to shop

--- World/shop_access.gd
+++ World/shop_access.gd
@@ -1,35 +1,26 @@
 extends Area2D
 
 var touching_player: bool
-var ready_to_enter: bool = false
-@onready var timer = $"../Timer"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ready_to_enter and touching_player and Input.is_action_pressed("Interact"):
-		get_tree().change_scene_to_file("res://World/level2.tscn")
+	if (touching_player && Input.is_action_pressed("Interact")):
+		get_tree().change_scene_to_file("res://World/Shop.tscn")
 	#print(touching_player)
-	pass
 
 
 func _on_body_entered(body):
+	print("Enter")
 	touching_player = true
-	
-	pass # Replace with function body.
-
 
 func _on_body_exited(body):
+	print("Exit")
 	touching_player = false
-	pass # Replace with function body.
 
 
-func _on_timer_timeout():
-	ready_to_enter = true
-	pass # Replace with function body.
+
