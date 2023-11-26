630cef4524869b08894f3ac36c0337ed168132fd
Theis Mathiassen <theis.mathiassen@gmail.com>
2023-11-26 05:41:11 +0100

    Remove used stuf from level 2

--- World/level2.gd
+++ World/level2.gd
@@ -1,9 +1,18 @@
 extends Node2D
 @onready var player = $player
 var hole_position = Vector2(396.2769, 581.6954)
+@onready var whiskey = $Whiskey
+@onready var bat = $bat
+@onready var gold = $Gold
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if (Global.whiskey_drunk):
+		whiskey.queue_free()
+	if (Global.bat_killed):
+		bat.queue_free()
+	if (Global.gold_mined):
+		gold.queue_free()
 	Global.is_in_cave = false
 	print("IN CAVE?" + str(Global.recently_in_cave))
 	if Global.recently_in_cave == true:		
@@ -14,4 +23,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (whiskey == null):
+		Global.whiskey_drunk = true
+	if (bat == null):
+		Global.bat_killed = true
+	if (gold == null):
+		Global.gold_mined = true
