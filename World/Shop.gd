e3c7ca278071051cf141d6b3a638cdfbec6377ab
Theis Mathiassen <theis.mathiassen@gmail.com>
2023-11-26 01:25:23 +0100

    Global upgrades

---
+++ World/Shop.gd
@@ -0,0 +1,35 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_cowboy_boots_pressed():
+	pass # Replace with function body.
+
+
+func _on_cowboy_vest_pressed():
+	pass # Replace with function body.
+
+
+func _on_goldminers_pick_pressed():
+	pass # Replace with function body.
+
+
+func _on_fireflies_pressed():
+	pass # Replace with function body.
+
+
+func _on_bullets_pressed():
+	pass # Replace with function body.
+
+
+func _on_sturdier_gun_pressed():
+	pass # Replace with function body.
